15cf029660372216eb16bb38c8f3107942c91c12
chirtu <chitufatuma45@gmail.com>
2024-09-10 20:28:26 +0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput],
+  schemas: [NO_ERRORS_SCHEMA],  // Add NO_ERRORS_SCHEMA here if needed
 })
 export class HomePage {
+  email: string = '';
+  password: string = '';
+
   constructor() {}
+
+  onLogin() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    // Handle login logic here
+  }
 }
